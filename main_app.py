b53cbc0ab599e4827f57164e50300cf04c24bca6
gecko1134 <85382061+gecko1134@users.noreply.github.com>
2025-05-21 09:11:45 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -158,14 +158,6 @@ BASE_DIR = os.path.dirname(__file__)
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-from ai_modules.demand_forecasting import DemandForecaster
-from ai_modules.scheduling_optimizer import optimize_schedule
-from ai_modules.sponsorship_matcher import match_sponsors
-from ai_modules.dynamic_contract_generator import generate_contract
-from ai_modules.membership_churn import ChurnPredictor
-from ai_modules.marketing_optimizer import optimize_campaign
-
-
 import optimize_schedule
 import match_sponsors
 import generate_contract
